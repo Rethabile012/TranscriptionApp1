d5ab1c896c39bf06d794bb2d18e439ecb4394280
Rethabile012 <rethabile.thabi01@gmail.com>
2025-10-25 20:00:26 +0200

    paths

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import os
 from extractedmfccs import AudioProcessor  
 
 class Dataset:
-    def __init__(self, audio_root="/content/transcriptions/Pipeline 2/TrainingSet/audio", transcript_root="/content/transcriptions/Pipeline 2/TrainingSet/transcripts"):
+    def __init__(self, audio_root="/kaggle/input/transcriptions/Pipeline 2/TrainingSet/audio", transcript_root="/kaggle/input/transcriptions/Pipeline 2/TrainingSet/transcripts"):
         self.audio_root = audio_root
         self.transcript_root = transcript_root
         self.processor = AudioProcessor(audio_root)
@@ -48,7 +48,7 @@ class Dataset:
 
         return dataset
 
-    def get_validation_data(self, audio_root="/content/transcriptions/Pipeline 2/ValidationSet/audio", transcript_root="/content/transcriptions/Pipeline 2/ValidationSet/transcripts"):
+    def get_validation_data(self, audio_root="/kaggle/input/transcriptions/Pipeline 2/ValidationSet/audio", transcript_root="/kaggle/input/transcriptions/Pipeline 2/ValidationSet/transcripts"):
         val_processor = AudioProcessor(audio_root)
         validation_data = []
 
